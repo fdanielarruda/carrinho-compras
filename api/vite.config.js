420885205e3c678d8856abd08d926b75d2635653
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-06 19:50:13 -0300

    feat: ajustando variaveis

--- api/vite.config.js
+++ api/vite.config.js
@@ -3,6 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://nginx',
+                changeOrigin: true,
+            },
+        },
+    },
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
