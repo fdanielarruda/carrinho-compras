0d406dbf9648d761025dcdbb6bce796ee6fc65fe
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-06 16:18:27 -0300

    feat: docker configurado para a api

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Configurando permissões das pastas de storage e cache..."
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+
+echo "Aguardando o serviço de banco de dados..."
+timeout=30
+while ! nc -z db 3306; do
+  sleep 1
+  timeout=$((timeout-1))
+  if [ "$timeout" -le 0 ]; then
+    echo "Erro: O banco de dados não está pronto após 30 segundos."
+    exit 1
+  fi
+done
+echo "Banco de dados pronto!"
+
+echo "Executando migrações..."
+php artisan migrate --force
+
+echo 'Executando Seeders...' &&
+php artisan db:seed --force &&
+
+exec "$@"
