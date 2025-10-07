2891edbc64c6b2c60dccfc9248ce3c77c3b86ab2
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-07 15:14:34 -0300

    fix: product seeder

--- api/database/seeders/ProductSeeder.php
+++ api/database/seeders/ProductSeeder.php
@@ -12,9 +12,9 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        if (DB::table('products')->count() > 0) {
-            return;
-        }
+        //if (DB::table('products')->count() > 0) {
+        //    return;
+        //}
 
         $defaultImage = "https://cdn.pixabay.com/photo/2017/09/23/04/02/dice-2777809_1280.jpg";
         $now = now();
