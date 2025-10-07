8daa9c0530b27f0c28b5dc822510a7383b822ed7
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-07 07:39:06 -0300

    feat: evitando seeders duplicadas

--- api/database/seeders/ProductSeeder.php
+++ api/database/seeders/ProductSeeder.php
@@ -12,6 +12,10 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
+        if (DB::table('products')->count() > 0) {
+            return;
+        }
+
         $defaultImage = "https://cdn.pixabay.com/photo/2017/09/23/04/02/dice-2777809_1280.jpg";
         $now = now();
 
